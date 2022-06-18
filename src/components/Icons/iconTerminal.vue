40182c3f3f6fd68673464a402b1f5fb58730d29e
Drew Harper <druharper@gmail.com>
2022-06-18 16:01:59 -0500

    save skills ani

--- src/components/Icons/iconTerminal.vue
+++ src/components/Icons/iconTerminal.vue
@@ -1,23 +1,31 @@
 <template>
   <svg viewBox="0 0 88 60" xmlns="http://www.w3.org/2000/svg">
-  <title>Command Line</title>
-  <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-        <g id="Artboard-Copy" transform="translate(-21.000000, -202.000000)" fill-rule="nonzero">
-            <g id="terminal" transform="translate(44.656320, 214.993171)">
-                <g id="Group" transform="translate(0.874329, 0.378060)" fill="#454545">
-                    <path d="M3.9712405,0.0312066941 L36.2553686,0.0312066941 C37.9122228,0.0312066941 39.2553686,1.37435244 39.2553686,3.03120669 L39.2553686,28.6555147 C39.2553686,30.3123689 37.9122228,31.6555147 36.2553686,31.6555147 L3.9712405,31.6555147 C2.31438625,31.6555147 0.971240502,30.3123689 0.971240502,28.6555147 L0.971240502,3.03120669 C0.971240502,1.37435244 2.31438625,0.0312066941 3.9712405,0.0312066941 Z" id="Rectangle-path"></path>
-                </g>
-                <polygon id="Shape" fill="#FFA700" points="13.3950309 9.56817395 26.9306305 15.7342732 13.3950309 22.0771552 13.3950309 25.3636116 30.580237 17.3476826 30.580237 14.1102142 13.3950309 6.08683053 13.3950309 9.56604404"></polygon>
-            </g>
+    <title>Command Line</title>
+    <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+      <g
+        id="Artboard-Copy"
+        transform="translate(-21.000000, -202.000000)"
+        fill-rule="nonzero"
+      >
+        <g id="terminal" transform="translate(44.656320, 214.993171)">
+          <g id="Group" transform="translate(0.874329, 0.378060)" fill="#454545">
+            <path
+              d="M3.9712405,0.0312066941 L36.2553686,0.0312066941 C37.9122228,0.0312066941 39.2553686,1.37435244 39.2553686,3.03120669 L39.2553686,28.6555147 C39.2553686,30.3123689 37.9122228,31.6555147 36.2553686,31.6555147 L3.9712405,31.6555147 C2.31438625,31.6555147 0.971240502,30.3123689 0.971240502,28.6555147 L0.971240502,3.03120669 C0.971240502,1.37435244 2.31438625,0.0312066941 3.9712405,0.0312066941 Z"
+              id="Rectangle-path"
+            ></path>
+          </g>
+          <polygon
+            id="Shape"
+            fill="#FFA700"
+            points="13.3950309 9.56817395 26.9306305 15.7342732 13.3950309 22.0771552 13.3950309 25.3636116 30.580237 17.3476826 30.580237 14.1102142 13.3950309 6.08683053 13.3950309 9.56604404"
+          ></polygon>
         </g>
+      </g>
     </g>
-</svg>
-
+  </svg>
 </template>
 
-<script>
-
-</script>
+<script></script>
 
 <style lang="scss" scoped>
   // @import '~/assets/css/_colors.scss';
@@ -25,7 +33,8 @@
   svg {
     // width: 88px;
     // height: 60px;
-
   }
-  svg g { cursor: pointer; }
+  svg:hover {
+    cursor: grab;
+  }
 </style>
--- src/components/Icons/iconVue.vue
+++ src/components/Icons/iconVue.vue
@@ -2,8 +2,8 @@
   <svg viewBox="0 0 88 60" xmlns="http://www.w3.org/2000/svg">
     <title>Vue.js</title>
     <g fill="none" fill-rule="evenodd">
-      <path d="M31.123 10.5H23l21 37 21-37h-8.402L43.861 32.943z" fill="#40b883"/>
-      <path d="M38.983 10.5H31l12.85 23L57 10.5h-7.737l-5.14 9.094z" fill="#34495e"/>
+      <path d="M31.123 10.5H23l21 37 21-37h-8.402L43.861 32.943z" fill="#40b883" />
+      <path d="M38.983 10.5H31l12.85 23L57 10.5h-7.737l-5.14 9.094z" fill="#34495e" />
     </g>
   </svg>
 </template>
@@ -19,5 +19,7 @@
       // fill: white;
     }
   }
-  svg g { cursor: pointer; }
+  svg:hover {
+    cursor: grab;
+  }
 </style>
