ed9dcc9b1f6ede3d8dfe8f3914c60a4e6debcd12
midlantica <druharper@gmail.com>
2022-10-27 11:44:10 -0500

    anis coming along

--- src/components/Icons/iconVue.vue
+++ src/components/Icons/iconVue.vue
@@ -1,9 +1,9 @@
 <template>
-  <svg viewBox="0 0 88 60" xmlns="http://www.w3.org/2000/svg">
+  <svg id="svgVue" viewBox="0 0 88 60" xmlns="http://www.w3.org/2000/svg">
     <title>Vue.js</title>
-    <g>
-      <path d="M31.123 10.5H23l21 37 21-37h-8.402L43.861 32.943z" fill="#40b883" />
-      <path d="M38.983 10.5H31l12.85 23L57 10.5h-7.737l-5.14 9.094z" fill="#34495e" />
+    <g class="groupVue">
+      <path class="darkGreen" d="M38.983 10.5H31l12.85 23L57 10.5h-7.737l-5.14 9.094z" />
+      <path class="lightGreen" d="M31.123 10.5H23l21 37 21-37h-8.402L43.861 32.943z" />
     </g>
   </svg>
 </template>
@@ -14,13 +14,35 @@
 svg {
   // width: 88px;
   // height: 60px;
-
-  .letterInsides {
-    // fill: white;
-  }
 }
 
 svg:hover {
-  cursor: grab;
+  cursor: pointer;
+}
+
+#svgVue {
+  //
+
+  &:hover .groupVue {
+    animation: iconFlipXAni 2s ease-in-out infinite;
+    transform-origin: 50% 50%;
+    animation-delay: 1s;
+  }
+}
+
+.groupVue {}
+
+.lightGreen {
+  fill: #40b883;
+}
+
+.darkGreen {
+  fill: #34495e;
+}
+
+@keyframes iconFlipXAni {
+  to {
+    transform: rotateY(360deg);
+  }
 }
 </style>
