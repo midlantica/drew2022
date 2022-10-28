a9fb244ad2e816621469508a443e53111fb0376a
midlantica <druharper@gmail.com>
2022-10-27 20:23:12 -0500

    save

--- src/components/Icons/iconNuxt.vue
+++ src/components/Icons/iconNuxt.vue
@@ -2,59 +2,58 @@
   <svg id="svgNuxt" viewBox="0 0 88 60" xmlns="http://www.w3.org/2000/svg">
     <title>Nuxt JS</title>
     <g class="groupNuxt">
-      <path fill-rule="evenodd" clip-rule="evenodd"
-        d="M41.833 13.9399C40.2963 11.3264 36.4543 11.3263 34.9175 13.9399L18.169 42.4236C16.6322 45.0371 18.5532 48.3041 21.6267 48.3041H34.7016C33.3882 47.1688 32.9019 45.2048 33.8957 43.5198L46.5803 22.0135L41.833 13.9399Z"
-        fill="#80EEC0" />
       <path
+        class="darkGreen"
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M41.833 13.9399C40.2963 11.3264 36.4543 11.3263 34.9175 13.9399L18.169 42.4236C16.6322 45.0371 18.5532 48.3041 21.6267 48.3041H34.7016C33.3882 47.1688 32.9019 45.2048 33.8957 43.5198L46.5803 22.0135L41.833 13.9399Z"
+      />
+      <path
+        class="lightGreen"
         d="M52.4398 20.6218C53.7419 18.5164 56.997 18.5164 58.299 20.6218L72.4891 43.567C73.7914 45.6723 72.1639 48.304 69.5599 48.304H41.1791C38.5751 48.304 36.9475 45.6723 38.2495 43.567L52.4398 20.6218Z"
-        fill="#00DC82" />
+      />
     </g>
   </svg>
 </template>
 
 <style lang="scss" scoped>
-// @import '~/assets/css/_colors.scss';
+  // @import '~/assets/css/_colors.scss';
 
-svg {
-  // width: 88px;
-  // height: 60px;
-}
-
-svg:hover {
-  cursor: pointer;
-}
-
-#svgNuxt {
-
-  //
-  &:hover .groupNuxt {
-    animation: iconFlipXAni 2s cubic-bezier(0.175, 0.885, 0.32, 1.275) 2;
-    transform-origin: 50% 50%;
-    animation-delay: .85s;
+  svg {
+    // width: 88px;
+    // height: 60px;
   }
-}
 
-.lightGreen {
-  fill: #00C58E;
-}
-
-.midGreen {
-  fill: #108775;
-}
-
-.darkGreen {
-  fill: #2F495E;
-}
-
-@keyframes iconFlipXAni {
-  to {
-    transform: rotateX(360deg);
+  svg:hover {
+    cursor: pointer;
   }
-}
 
-@keyframes iconFlipYAni {
-  to {
-    transform: rotateY(360deg);
+  #svgNuxt {
+    //
+    &:hover .groupNuxt {
+      animation: iconFlipXAni 2s cubic-bezier(0.175, 0.885, 0.32, 1.275) 2;
+      transform-origin: 50% 50%;
+      animation-delay: 0.85s;
+    }
   }
-}
+
+  .lightGreen {
+    fill: #00dc82;
+  }
+
+  .darkGreen {
+    fill: #80eec0;
+  }
+
+  @keyframes iconFlipXAni {
+    to {
+      transform: rotateX(360deg);
+    }
+  }
+
+  @keyframes iconFlipYAni {
+    to {
+      transform: rotateY(360deg);
+    }
+  }
 </style>
