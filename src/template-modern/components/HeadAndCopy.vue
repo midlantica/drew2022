14cfc3392cb9f698d169f983b29d50748e006f2a
midlantica <druharper@gmail.com>
2023-05-10 17:33:07 -0500

    save

--- src/template-modern/components/HeadAndCopy.vue
+++ src/template-modern/components/HeadAndCopy.vue
@@ -2,7 +2,9 @@
   <div>
     <div class="grid grid-cols-[88px_auto] auto-rows-auto gap-x-1.5 justify-items-start items-center mt-4 mb-3 gap-2">
       <router-link :to="{ name: 'ModernExtras' }" class="col-start-1 col-end-2 row-start-1 row-end-2 bg-no-repeat bg-cover w-[78px] h-[94px] self-start bg-[url('/img/drew_mug_red.png')] jelloHorizontal" />
-      <h1>{{ store.copy.druHead }}</h1>
+      <h1 class="col-start-2 col-end-3 row-start-1 row-end-2  inline-block font-modernSubhead tracking-[-0.01ch] p-0 self-center mt-[2.2rem] w-[20ch] text-white font-normal text-[1.1rem] drop-shadow-[1px_1px_4px_black]">
+        {{ store.copy.druHead }}
+      </h1>
     </div>
     <p class="font-light text-[0.95rem] leading-[2.2rem] tracking-[0.03rem] m-0 font-modernCopy text-white" v-html="store.copy.druCopy" />
   </div>
@@ -17,7 +19,7 @@
 <style lang="scss" scoped>
 
   h1 {
-    @apply col-start-2 col-end-3 row-start-1 row-end-2  inline-block font-modernSubhead tracking-[-0.01ch] p-0 self-center mt-[2.2rem] w-[20ch] text-white font-normal text-[1.1rem] drop-shadow-[1px_1px_4px_black];
+    // @apply col-start-2 col-end-3 row-start-1 row-end-2  inline-block font-modernSubhead tracking-[-0.01ch] p-0 self-center mt-[2.2rem] w-[20ch] text-white font-normal text-[1.1rem] drop-shadow-[1px_1px_4px_black];
 
     @media (max-width: 535px) {
       @apply mt-[.75rem] w-auto;
--- src/template-modern/components/ModernTabs.vue
+++ src/template-modern/components/ModernTabs.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="flex flex-row flex-wrap justify-center gap-4 mx-0 mb-3 font-modernCopy mt-7">
     <router-link
-      to="/Modern/Projects" class="text-black pt-[.55rem] px-5 pb-[.4rem] rounded-[2rem] no-underline uppercase text-[.6rem] font-semibold tracking-[0.075ch] bg-modern-ruby bg-gradient-to-b from-[#D71D5B] to-[#871239] hover:bg-modern-ruby hover:text-white hover:bg-gradient-to-b hover:from-[#D71D5B] hover:to-[#871239]"
+      to="/Modern/Projects" class="projectsTab text-black pt-[.55rem] px-5 pb-[.4rem] rounded-[2rem] no-underline uppercase text-[.6rem] font-semibold tracking-[0.075ch] bg-modern-ruby bg-gradient-to-b from-[#D71D5B] to-[#871239] hover:bg-modern-ruby hover:text-white hover:bg-gradient-to-b hover:from-[#D71D5B] hover:to-[#871239]"
     >
       Projects
     </router-link>
     <router-link
-      to="/Modern/Skills" class="text-black pt-[.55rem] px-5 pb-[.4rem] rounded-[2rem] no-underline uppercase text-[.6rem] font-semibold tracking-[0.075ch] bg-modern-ruby bg-gradient-to-b from-[#D71D5B] to-[#871239] hover:bg-modern-ruby hover:text-white hover:bg-gradient-to-b hover:from-[#D71D5B] hover:to-[#871239]"
+      to="/Modern/Skills" class="skillsTab text-black pt-[.55rem] px-5 pb-[.4rem] rounded-[2rem] no-underline uppercase text-[.6rem] font-semibold tracking-[0.075ch] bg-modern-ruby bg-gradient-to-b from-[#D71D5B] to-[#871239] hover:bg-modern-ruby hover:text-white hover:bg-gradient-to-b hover:from-[#D71D5B] hover:to-[#871239]"
     >
       Skills
     </router-link>
     <router-link
-      to="/Modern/About" class="text-black pt-[.55rem] px-5 pb-[.4rem] rounded-[2rem] no-underline uppercase text-[.6rem] font-semibold tracking-[0.075ch] bg-modern-ruby bg-gradient-to-b from-[#D71D5B] to-[#871239] hover:bg-modern-ruby hover:text-white hover:bg-gradient-to-b hover:from-[#D71D5B] hover:to-[#871239]"
+      to="/Modern/About" class="aboutTab text-black pt-[.55rem] px-5 pb-[.4rem] rounded-[2rem] no-underline uppercase text-[.6rem] font-semibold tracking-[0.075ch] bg-modern-ruby bg-gradient-to-b from-[#D71D5B] to-[#871239] hover:bg-modern-ruby hover:text-white hover:bg-gradient-to-b hover:from-[#D71D5B] hover:to-[#871239]"
     >
       About
     </router-link>
@@ -21,22 +21,10 @@
 <script setup></script>
 
 <style lang="scss" scoped>
-
   body.modern.projects a.projectsTab,
   body.modern.skills a.skillsTab,
   body.modern.about a.aboutTab {
     @apply text-white;
   }
 
-  .modernTabs {
-    // @apply font-modernCopy mt-7 mx-0 mb-3 flex flex-row flex-wrap justify-center;
-
-    a {
-      // @apply text-black my-2 mx-2 pt-[.55rem] px-5 pb-[.4rem] rounded-[2rem] no-underline uppercase text-[.6rem] font-semibold tracking-[0.075ch] bg-modern-ruby bg-gradient-to-b from-[#D71D5B] to-[#871239];
-
-      &:hover {
-        @apply bg-modern-ruby text-white bg-gradient-to-b from-[#D71D5B] to-[#871239];
-      }
-    }
-  }
 </style>
